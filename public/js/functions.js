29f6dd548b16b3fe2171405919e82fe0b90a364c
xinodeprinz <xinodeprinz@gmail.com>
2023-03-09 14:53:27 +0100

    Added language sweetalert

--- public/js/functions.js
+++ public/js/functions.js
@@ -19,3 +19,36 @@ function loadSpinner(btn) {
     btn.innerHTML = `
     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`
 }
+
+window.onload = () => {
+    if (!sessionStorage.getItem('locale')) {
+        const data = {
+            en: {
+                title: '<div style="font-size:22px">Select Language</div>'
+            },
+            fre: {
+                title: '<div style="font-size:25px">Choisir La Langue</div>'
+            },
+            colors: {
+                main: '#356735',
+            },
+        }
+        Swal.fire({
+            title: `<div style="color:${data.colors.main}">${data.fre.title}${data.en.title}</div>`,
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: 'Français<br/>French',
+            denyButtonText: `Anglais<br/>English`,
+            confirmButtonColor: data.colors.main,
+            denyButtonColor: 'black',
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                window.location.pathname = '/language/fre';
+            } else if (result.isDenied) {
+                window.location.pathname = '/language/en';
+            }
+            sessionStorage.setItem('locale', true);
+        });
+    }
+}
